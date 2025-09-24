772d5af3c6fe3b0b4f6bc8904e3736577afec90e
officialkkyshimla <kkyshimlatechguy@gmail.com>
2025-09-24 20:40:46 +0530

    Create dimensionalweight.c

---
+++ dimensionalweight.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+  int height,width,length,volume,weight;
+  printf("Enter the height of box\n");
+  scanf("%d",&height);
+  printf("Enter the width of box\n");
+  scanf("%d",&width);
+  printf("Enter the length of box\n");
+  scanf("%d",&length);
+  volume = height * width * length;
+  weight = (volume+165)/166;
+  printf("The Volume of the Box is : %d\n",volume);
+  printf("The Weight of the box is : %d",weight);
+  return 0;
+}
+
+  
